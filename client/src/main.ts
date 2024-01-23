f887405e39ac85eaeddb7290c8b35b7915237bd5
skystar200 <skystar20095@gmail.com>
2024-01-23 16:58:26 +0900

    add /user, db column

--- client/src/main.ts
+++ client/src/main.ts
@@ -2,8 +2,10 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common'; 
 
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors(); //다른 자원에서 접근할 수 있는 권한 부여
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
     forbidNonWhitelisted: true,
