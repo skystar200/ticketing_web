ae6eb4894c4166c4d29727568ae2f1b471453481
BYEOL <80840476+skystar200@users.noreply.github.com>
2024-01-26 19:10:54 +0900

    Update app.module.ts

--- client/src/app.module.ts
+++ client/src/app.module.ts
@@ -13,7 +13,7 @@ import { UserModule } from './user/user.module';
       host: 'localhost',
       port: 3306,
       username: 'root',
-      password: 'kyk12050713~',
+      password: ,
       database: 'ticketing_web',
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize: true,
